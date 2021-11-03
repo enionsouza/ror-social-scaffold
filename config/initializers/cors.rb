74c5d2f1d9e5b3d4f56e1c6c3741f4792965c87f
mariordgez <mariordgez@hotmail.com>
2021-11-03 12:00:25 -0700

    Add migration file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,1 +1,6 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do allow do origins '*' resource( '*', headers: :any, expose: ["Authorization"], methods: [:get, :patch, :put, :delete, :post, :options, :show] ) end end 
+Rails.application.config.middleware.insert_before 0, Rack::Cors do 
+  allow do 
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :patch, :put, :delete, :post, :options, :head, :show]
+    end 
+  end 
