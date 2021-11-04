6eaab8bb7e6159e956ae29ced84ebdd988f458ba
Enio N. de Souza <enionsouza@gmail.com>
2021-11-04 13:59:20 -0400

    Fix Linters

--- Gemfile
+++ Gemfile
@@ -42,8 +42,6 @@ gem 'devise'
 gem 'devise-jwt'
 gem 'rack-cors'
 
-gem 'hirb'
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -2,11 +2,11 @@ module Response
   def multi_response(object)
     respond_to do |format|
       format.html
-      format.json { render :json => object }
+      format.json { render json: object }
     end
   end
 
   def json_response(object)
-    render :json => object
+    render json: object
   end
 end
