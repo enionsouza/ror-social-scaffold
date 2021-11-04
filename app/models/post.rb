cd8928dddbd61d6dbf75d6834121cee47aff8579
Ángel Díaz <diazangel9311@gmail.com>
2021-11-04 10:17:37 -0500

    Create a new post with current logged-in user

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
                                                 too_long: '1000 characters in post is the maximum allowed.' }
 
   belongs_to :user
-
+  # validates :content, uniqueness: true
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
